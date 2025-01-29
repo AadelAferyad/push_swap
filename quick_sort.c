aca986efba992baad5979aa16084d365cdd87865
aadelAferyad <animevineu@gmail.com>
2025-01-29 13:53:31 +0100

    implementing new algo (the turk algo)

--- quick_sort.c
+++ quick_sort.c
@@ -13,85 +13,44 @@
 #include "main.h"
 #include <stdio.h>
 
-int	closest_operation(stack *stk, int node, int stk_size)
+int	closest_operation(stack *stack_a, stack *stack_b, int index)
 {
+	stack	*head;
+	int	size;
 	int	i;
 
 	i = 0;
-	if (!stk)
-		return (10);
-	if (stk->index == node)
-		return (0);
-	if (stk->next && stk->next->index == node)
-		return (1);
-	while (stk)
+	size = stack_size(stack_a);
+	head = stack_a;
+	while (head)
 	{
-		if (stk->index == node)
+		if (head->index == index)
 			break ;
-		stk = stk->next;
 		i++;
+		head = head->next;
 	}
-	if (i < (stk_size / 2))
-		return (2);
-	return (3);
-}
-int	max_index(stack *stk)
-{
-	int	index;
 
-	index = stk->index;
-	while (stk)
+}
+
+void	set_operations(stack *stack_a, stack *stack_b)
+{
+	while (stack_a)
 	{
-		if (stk->index > index)
-			index = stk->index;
-		stk = stk->next;
+		stack_b->op = closest_operation(stack_a, stack_b, stack_a->index);
+		
 	}
-	return (index);
 }
 
-
-void	sorted_stack_a(stack **stack_a, stack **stack_b, int size)
+void	find_best_op(stack **stack_a, stack **stack_b)
 {
-	int	index;
-	int	op;
+	stack	*head;
 
-	index = max_index(*stack_b);
-	while (*stack_b)
+	head = *stack_a;
+	unset_operation(*stack_a);
+	set_operations(*stack_a, *stack_b);
+	while (head)
 	{
-		op = closest_operation(*stack_b, index, size);
-		print_stack(*stack_b);
-		printf("stack a indexx : %d\n", index);
-		print_stack(*stack_a);
-		if (!op)
-		{
-			push_stack(stack_b, stack_a, PA);
-			index--;
-		}
-		else if (op == 1)
-			swap_stack(stack_b, SB);
-		else if (op == 2)
-			rotate_stack(stack_b, RB);
-		else if (op == 3)
-			reverse_rotate_stack(stack_b, RRB);
 	}
-}
-/*
- * (0)-->(2)-->(1)-->
- * */
-void	optimize_sort(stack **stk, int low, int high, int size)
-{
-	int	op;
-	int	op1;
-
-	op = closest_operation(*stk, low, size);
-	op1 = closest_operation(*stk, high, size);
-
-	if (op == 1 || op1 == 1)
-		swap_stack(stk, SA);
-	else if (op == 2 || op1 == 2)
-		rotate_stack(stk, RA);
-	else if (op == 3 || op1 == 3)
-		reverse_rotate_stack(stk, RRA);
 }
 
 void	sort_stack(stack **stack_a, stack **stack_b)
@@ -103,5 +62,6 @@ void	sort_stack(stack **stack_a, stack **stack_b)
 	size = stack_size(*stack_a);
 	while (size > 3)
 	{
+		find_best_op(stack_a, stack_b);
 	}
 }
